5512a2c53d067efdc1a4160b7b7aa5303284e80b
tienvu95 <tienvu95@gmail.com>
2023-02-02 13:23:57 +0700

    exp with data

--- utility.py
+++ utility.py
@@ -411,7 +411,7 @@ def ranking_analysis(Rec, test_df, train_df, key_genre, item_idd_genre_list, use
 
 def relative_std(dictionary):
     tmp = []
-    for key, value in sorted(dictionary.iteritems(), key=lambda (k, v): (v, k)):
+    for key, value in sorted(dictionary.iteritems(), key = lambda x: x[0]):
         tmp.append(value)
     rstd = np.std(tmp) / (np.mean(tmp) + 1e-10)
     return rstd
